45dcb59492ee33709f3140849e1d9a4888ea4023
Yaamiilaa <yamilaramoshdez@gmail.com>
2023-06-05 17:10:25 +0100

    Se realiza el primer proyecto de JavaScript

--- JavaScript/Project1/src/js/main.js
+++ JavaScript/Project1/src/js/main.js
@@ -1,14 +1,15 @@
 window.onload = () => {
     detectColor();
-    randomColor();
 }
+
+const colors_index = ['green', 'black', 'yellow', 'red', 'blue']
 function detectColor() {
     var buttons = document.querySelectorAll('#container div');
-    var colors_index = ['0', '1', '2', '3', '4']
+
     for (var i = 0; i < buttons.length; i++) {
         var button = buttons[i];
         button.addEventListener("mouseover", function () {
-            changeBgColor(this.id);
+            changeRandomColor(this);
 
         })
     }
@@ -18,7 +19,7 @@ function changeBgColor(color) {
     document.getElementsByTagName("body")[0].style.backgroundColor = color;
 }
 
-function changeRandomColor(color) {
-    document.getElementsByTagName('body')[0].style.backgroundColor = Math.floor(Math.random(colors_index))
+function changeRandomColor(cell) {
+    cell.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
 }
 
